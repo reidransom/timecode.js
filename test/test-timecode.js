c41c92aaf0b9628e1e01f308b6195abaea395b11
Reid Ransom <reid@reidransom.com>
2012-11-28 22:50:40 -0800

    Added "add" and "subtract" methods

--- test/test-timecode.js
+++ test/test-timecode.js
@@ -16,7 +16,22 @@ exports["Timecode"] = function(test) {
     test.equal(0, tc.seconds);
     test.equal(0, tc.frames);
     test.equal(108000, tc.frame_count);
-    
+    tc.add(1);
+    test.equal("01:00:00:01", tc.toString());
+    tc.add(30);
+    test.equal("01:00:01:01", tc.toString());
+    tc.add(30*60);
+    test.equal("01:01:01:01", tc.toString());
+    tc.add(30*3600);
+    test.equal("02:01:01:01", tc.toString());
+    tc.add(timecode.init({framerate: "29.97", timecode: "01:00:00:00"}));
+    test.equal("03:01:01:01", tc.toString());
+    tc.add("00:33:22:11");
+    test.equal("03:34:23:12", tc.toString());
+    tc.subtract("00:00:00:01");
+    test.equal("03:34:23:11", tc.toString());
+    test.throw()
+
     tc = timecode.init({
         framerate: "29.97",
         timecode: 108000,
@@ -29,6 +44,14 @@ exports["Timecode"] = function(test) {
     test.equal(0, tc.seconds);
     test.equal(0, tc.frames);
     test.equal(108000, tc.frame_count);
+    tc.add(1);
+    test.equal("01:00:00:01", tc.toString());
+    tc.add(30);
+    test.equal("01:00:01:01", tc.toString());
+    tc.add(30*60);
+    test.equal("01:01:01:01", tc.toString());
+    tc.add(30*3600);
+    test.equal("02:01:01:01", tc.toString());
     
     tc = timecode.init({
         framerate: "23.98",
