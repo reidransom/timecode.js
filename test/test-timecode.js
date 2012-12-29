906e44d5521e2c953e2ed0b546f03b6bfb7de08e
Reid Ransom <reid@reidransom.com>
2012-12-28 20:08:33 -0800

    working on dropframe

--- test/test-timecode.js
+++ test/test-timecode.js
@@ -3,7 +3,8 @@ var assert = require("assert"),
 
 exports["Timecode"] = function(test) {
     var tc;
-    
+
+/*    
     tc = timecode.init({
         framerate: "29.97",
         timecode: "01:00:00:00",
@@ -30,7 +31,7 @@ exports["Timecode"] = function(test) {
     test.equal("03:34:23:12", tc.toString());
     tc.subtract("00:00:00:01");
     test.equal("03:34:23:11", tc.toString());
-    test.throw()
+    //test.throw();
 
     tc = timecode.init({
         framerate: "29.97",
@@ -79,5 +80,77 @@ exports["Timecode"] = function(test) {
     test.equal(0, tc.frames);
     test.equal(86400, tc.frame_count);
     
+    tc = timecode.init({
+        framerate: "29.97",
+        //timecode: 108000,
+        timecode: "01:00:00;00",
+        drop_frame: true
+    });
+    test.equal("29.97", tc.framerate);
+    test.equal(30, tc.int_framerate);
+    test.equal(true, tc.drop_frame);
+    test.equal(1, tc.hours);
+    test.equal(0, tc.minutes);
+    test.equal(0, tc.seconds);
+    test.equal(0, tc.frames);
+    test.equal(107892, tc.frame_count);
+    test.equal("01:00:00;00", tc.toString());
+    tc.add(1);
+    test.equal("01:00:00;01", tc.toString());
+*/
+/*
+    tc.add(30);
+    test.equal("01:00:01;01", tc.toString());
+    tc.add(30*60);
+    test.equal("01:01:01;01", tc.toString());
+    tc.add(30*3600);
+    test.equal("02:01:01;01", tc.toString());
+*/
+
+    tc = timecode.init({
+        framerate: "29.97",
+        timecode: "01:00:00;00",
+        drop_frame: true
+    });
+    test.equal("29.97", tc.framerate);
+    test.equal(30, tc.int_framerate);
+    test.equal(true, tc.drop_frame);
+    test.equal(1, tc.hours);
+    test.equal(0, tc.minutes);
+    test.equal(0, tc.seconds);
+    test.equal(0, tc.frames);
+    test.equal(107892, tc.frame_count);
+    test.equal("01:00:00;00", tc.toString());
+    //tc.add(30*60);
+    tc.add("00:01:00;00");
+    //test.equal("01:01:00;02", tc.toString());
+    tc.add("00:30:00;00");
+    //test.equal("01:31:00;02", tc.toString());
+    
+    tc = timecode.init({
+        framerate: "29.97",
+        timecode: "01:00:01;00",
+        drop_frame: true
+    });
+    test.equal("29.97", tc.framerate);
+    test.equal(30, tc.int_framerate);
+    test.equal(true, tc.drop_frame);
+    test.equal(1, tc.hours);
+    test.equal(0, tc.minutes);
+    test.equal(1, tc.seconds);
+    test.equal(0, tc.frames);
+    test.equal(107922, tc.frame_count);
+    test.equal("01:00:01;00", tc.toString());
+    //tc.add(30*60);
+    tc.add("00:01:00;00");
+    //test.equal("01:01:01;00", tc.toString());
+    tc.add("00:30:00;00");
+    //test.equal("01:31:01;00", tc.toString());
+
+    console.log(tc.frameNumberToDropFrameTimecode(108000));
+    console.log(tc.dropFrameTimecodeToFrameNumber([1, 0, 3, 18]));
+
     test.done();
+
+
 };
